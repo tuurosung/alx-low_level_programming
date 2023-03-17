3f47b941c37f37718668865efb302ed9f9313c2b
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-03-17 04:50:35 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - display the given string in when put function is called
+*
+* Description: the main function of
+* this program is to print the given text "Programming is like building a
+* multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
