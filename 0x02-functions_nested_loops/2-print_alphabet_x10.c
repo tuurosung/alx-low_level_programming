ffe66505f5f0dd0d592f69995c62eb3b39d17a22
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-03-21 23:33:52 +0000

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Program prints alphabets in lower case 10x
+ *
+ * Description: when called, the main function returns
+ * all lowercase alphabets tentimes followed by a new line
+ * Return: 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	
+
+	_putchar('\n');
+	}
+}
