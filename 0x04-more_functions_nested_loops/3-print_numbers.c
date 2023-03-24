dc3f7c6b848e6e03c2e8c7d4f6047828f86e5925
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-03-24 00:30:13 +0000

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ *
+ */
+void print_numbers(void)
+{
+	char c;
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
