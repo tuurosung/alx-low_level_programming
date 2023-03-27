6bfc5a4d3d2e3a3bc61e854922538bece1cad2ea
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-03-27 23:15:16 +0000

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates
- * 		 the value it points to 98
+ * the value it points to 98
  *
  * @n: The pointer to the integer (int)
  *
