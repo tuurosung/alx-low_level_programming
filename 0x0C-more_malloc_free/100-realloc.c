f36c4779fbfbe50e4e8c5558cfdb0f74dd30c139
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-04-14 09:59:00 +0000

    Betty corrections more than 40 lines error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,7 +4,6 @@
 
 /**
  * _realloc - function that reallocates a memory block using malloc and free
- *
  * @ptr: a pointer to the memory prev allocated
  * @old_size: previously allocated memory size for ptr in bytes
  * @new_size: the new allocated memory block
@@ -17,8 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	/* initialize variables and counter */
 	void *m;
 	unsigned int i;
-	char *new_mem;
-	char *holder;
+	char *new_mem, *holder;
 
 	/* check for arguments */
 	if (new_size == old_size)
@@ -27,15 +25,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		m = malloc(new_size);
-
-		/* new_mem cannot be NULL */
 		if (m == NULL)
 			return (NULL);
 
 		return (m);
 	}
 
-	/* if new_size is 0 and pointer is not NULL */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -43,8 +38,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	new_mem = ptr;
-
-	/* allocate memory */
 	m = malloc(sizeof(*new_mem) * new_size);
 
 	/* check for NULL */
@@ -55,8 +48,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	holder = m;
-
-	/* loop to create memory block */
 	for (i = 0; i < old_size && i < new_size; i++)
 		holder[i] = *new_mem++;
 
