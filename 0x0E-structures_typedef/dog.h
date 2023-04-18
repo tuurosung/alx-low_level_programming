4ef503349596206272e91a920e8be2fee7f7af72
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-04-18 02:09:57 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - definition of an instance of dog
+ *
+ * @name: is the name of the dog
+ * @age: is the age of the dog
+ * @owner: is the owner of the dog
+ */
+
+struct dog
+{
+	/* elements of new dog type */
+	char *name;
+	float age;
+	char *owner;
+};
+
