3277f584ab2366d6ba051d8a1c964fafc6a106f5
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-04-29 22:48:12 +0000

    Advanced task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) printHare(void);
+
+/**
+ * printHare - a function that prints a given string
+ *
+ */
+
+void printHare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back\n");
+}
