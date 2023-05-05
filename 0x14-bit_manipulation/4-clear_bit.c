58387d55a62cbcc1aa9397097f592dc260a7a6e7
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-05-05 00:10:33 +0000

    Correction 4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	/* check for size of bit */
-	if (index >= (sizeof(unsigned long int) * *))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	*n &= ~(1 << index);
