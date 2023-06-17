d61c533265660f1a2d11ff23a82c922ed6b16b2c
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-06-17 02:18:17 +0000

    solve task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,20 +9,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int c = 0;
+	size_t c;
 
-	/* check for NULL */
-	if (h == NULL)
-		return (c);
-
-	while (h->prev != NULL)
-		h=h->prev;
+	c = 0;
 
 	while (h != NULL)
 	{
 		c++;
 		printf("%d\n", h->n);
 		h = h->next;
+		c++;
 	}
 
 	return (c);
