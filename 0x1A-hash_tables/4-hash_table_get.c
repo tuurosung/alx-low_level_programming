637e5a97fe3298767abb8e8682af70fea9276e91
Shahabdeen <tuurosungshahabdeen@yahoo.com>
2023-07-06 23:09:51 +0000

    Solve 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Get a hash table
+ * @ht: address of the hash table
+ * @key: key of the hash table
+ * Return: Value of the key or NULL if not found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
